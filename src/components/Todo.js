3ed93b8ea215590bf57a37d7b12a3bda36797130
s15613 <dutaitu92@gmail.cogit config --global user.name s15613git statusqgitgit commit -m 'git test'git commit -m 'git test'git config --global user.name s15613git config --global user.email dutaitu92@gmail.co>
2019-06-27 17:38:53 +0900

    updated

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -6,12 +6,12 @@ class Todo extends React.Component {
             <>
             <li>
             <i class="fas fa-check-circle mi non"></i>
-                <span className="title done">dfadf</span>
+                <span className="title done">Go to bed</span>
                 <span className="close">&#10799;</span>
             </li>
             <li>
                 <i class="far fa-check-circle mi"></i>
-                <span>dfadf</span>
+                <span>Go to Tran Duy Hung Street</span>
                 <span className="close">&#10799;</span>
             </li>
             </>
